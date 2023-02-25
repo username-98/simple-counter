4937707008ec9231a3925bd91275bd1901fe323d
username98 <ay.amouch@gmail.com>
2023-02-25 23:24:44 +0000

    added : save function

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-
-
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
